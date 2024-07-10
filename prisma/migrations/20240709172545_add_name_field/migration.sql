996b53bfe887b97f12ab8af91abb257f3cc6c8c9
tanishqnirwan <tanishq9304@gmail.com>
2024-07-10 07:06:01 +0530

    chore: Update package.json and prisma schema

--- prisma/migrations/20240709172545_add_name_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `name` to the `Referral` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `referral` ADD COLUMN `name` VARCHAR(191) NOT NULL;
--- prisma/migrations/20240709172829_add_name_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `name` on the `referral` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `referral` DROP COLUMN `name`;
